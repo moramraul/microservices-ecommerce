2224113abb10d792f204b9281a0c09f4920e8c88
moramraul <94130850+moramraul@users.noreply.github.com>
2022-06-20 19:58:17 +0200

    avances

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,21 +8,44 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
-const app = express()
+const cookieSession = require('cookie-session')
+import mongoose from "mongoose";
+const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
-app.all("*", ()=>{
+app.all("*", () => {
     throw new NotFoundError()
 })
 app.use(errorHandler)
 
 
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined')
+    }
 
-app.listen(3000, () => {
-    console.log("Listening on 3000")
-})
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth",);
+        console.log("Conected to MongoDB")
+    }
+    catch (err) {
+        console.error(err);
+        console.log("Failed to connect to DB")
+    }
+    app.listen(3000, () => {
+        console.log("Listening on 3000")
+    })
+}
+
+start()
+
 
